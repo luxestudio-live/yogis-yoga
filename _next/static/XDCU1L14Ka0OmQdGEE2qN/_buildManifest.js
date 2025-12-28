3e1dc1a5d55fc35e499fe34bcd5bba1393c12d18
the-rahulk <the-rahulk@users.noreply.github.com>
2025-12-28 17:41:35 +0000

    deploy: 3d3cf6d12c4633c565ba6df5f1021473f0354242

--- _next/static/XDCU1L14Ka0OmQdGEE2qN/_buildManifest.js
+++ _next/static/XDCU1L14Ka0OmQdGEE2qN/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/yogis-yoga//_next/:path+",
-        "destination": "/yogis-yoga/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
